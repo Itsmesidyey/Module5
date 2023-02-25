366b8f1670abc7e3c59b38d8171ed7472513bc03
Itsmesidyey <119683075+Itsmesidyey@users.noreply.github.com>
2023-02-25 16:24:51 +0800

    Unit-Test

---
+++ Module6_Unit_Test_Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Module6_Unit_Test_Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Module6_Unit_Test_Project")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("187d7ced-b59b-431d-a0c4-0096fa78ed61")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
